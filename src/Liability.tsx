aadaf6dd7411d6089895d73f7e484affd659053b
Mac Groves <mgroves@averoinc.com>
2019-12-15 19:10:17 -0700

    add more formatting

--- src/Liability.tsx
+++ src/Liability.tsx
@@ -3,25 +3,32 @@ import { Typography } from "@material-ui/core";
 
 export const Liability = () => {
   return (
-    <Typography align="justify" variant="body2" color="textSecondary">
-      Taco Grille LLC &copy; (referred to as the Company) does not promise that
-      the information provided on this webpage is accurate or up to date. The
-      Company is not liable for user attempts to drive on I-80 in inclement
-      weater, not to drive on I-80 in good weather, or unwarranted Pisten Bully
-      purchases. Furthermore, the Company is not responsible at all for decided
-      whether or not I-80 is open, so please do not call the Company with
-      questions regarding the motives of I-80 closures or forecasts for road
-      reopening / reclosing. Representatives of the Company will also decline
-      requests for comments on the ongoing WY Supreme Court case Town of Rock
-      Springs Highway Department vs Taco Grill LLC, which is an egregious misuse
-      of tax-payer dollars, and a laughable weak accusation against an
-      upstanding not-for-profit NGO concerning itself with the well-being of its
-      shareholders and the management of the Taco Grill Estate &reg;. This site
-      is for non-commercial use only. Users seeking to profit off of the
-      algorithm contained herein will be prosecuted to the fullest extent of the
-      law. Law abiding citizens seeking to use our Artifical Intelligence API
-      backed with state-of-the-art Machine Learning algorithms can contact the
-      company for pricing information.
-    </Typography>
+    <>
+      <Typography align="justify" variant="body2" color="textSecondary">
+        Taco Grille LLC &copy; (referred to as the Company) does not promise
+        that the information provided on this webpage is accurate or up to date.
+        The Company is not liable for user attempts to drive on I-80 in
+        inclement weater, not to drive on I-80 in good weather, or unwarranted
+        Pisten Bully purchases. Furthermore, the Company is not responsible at
+        all for deciding whether or not I-80 is open, so please do not call the
+        Company with questions regarding the motives of I-80 closures or
+        forecasts for road reopening / reclosing.
+      </Typography>
+      <Typography align="justify" variant="body2" color="textSecondary">
+        Representatives of the Company will also decline requests for comments
+        on the ongoing WY Supreme Court case Town of Rock Springs Highway
+        Department vs Taco Grill LLC, which is an egregious misuse of tax-payer
+        dollars, and a laughably weak accusation against an upstanding
+        not-for-profit NGO concerning itself with the well-being of its
+        shareholders and the management of the Taco Grill Estate &reg;.
+      </Typography>
+      <Typography align="justify" variant="body2" color="textSecondary">
+        This site is for non-commercial use only. Users seeking to profit off of
+        the algorithm contained herein will be prosecuted to the fullest extent
+        of the law. Law abiding citizens seeking to use our Artifical
+        Intelligence API backed with state-of-the-art Machine Learning
+        algorithms can contact the company for pricing information.
+      </Typography>
+    </>
   );
 };
